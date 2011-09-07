b77738407f48be17a2c781e9ab03b4a390feafd4
Oleksandr Bodnarashyk <boda2004@gmail.com>
2011-09-07 11:25:28 +0300

    functions moved to separate file from config

--- config.php
+++ config.php
@@ -24,39 +24,6 @@ if (is_readable(DROOT.'/config/'.$host.'.php')) {
     require_once DROOT.'/config/'.$host.'.php';
 }
 
-
-function severity2string($severity) {
-    switch($severity) {
-        case SEVERITY_CRIT:
-            return 'crit';
-        case SEVERITY_WARN:
-            return 'warn';
-        case SEVERITY_DEBUG:
-            return 'debug';
-        default:
-            return 'unknown';
-    }
-}
-
-function add_to_log($string, $severity = SEVERITY_DEBUG) {
-    $log = $GLOBALS['CONFIG']['LOG'];
-    if ($severity < $log['severity']) {
-        return;
-    }
-    if (is_array($string)) {
-        $string = print_r($string, true);
-    }
-    $string = trim(strtr($string, "\n", " "));
-    $f = fopen($log['path'].'/'.date('Ymd').'.log', 'a');
-    fwrite($f, '['.date("r").'] ['.severity2string($severity).'] '.$string."\n");
-    fclose($f);
-}
-
-function __class_loader($class) {
-    if (is_readable(DROOT.'/lib/'.$class.'.php')) {
-        require_once DROOT.'/lib/'.$class.'.php';
-    }
-}
+require_once DROOT.'/lib/functions.php';
 spl_autoload_register('__class_loader');
-
 ?>
