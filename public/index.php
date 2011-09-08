414d92153e2b355f2112588e2c5ea6873d828c30
Oleksandr Bodnarashyk <boda2004@gmail.com>
2011-09-08 15:10:00 +0300

    redirect url now taken from campaign, if detected. From carrier otherwise.

--- public/index.php
+++ public/index.php
@@ -2,7 +2,19 @@
 require_once '../config.php';
 $resolver = new Resolver();
 $carrierRow = $resolver->carrierRowByIp();
-add_to_log($carrierRow, SEVERITY_DEBUG);
-$url = $carrierRow['redirect_url'];
+$campaign = empty($_REQUEST['campaign'])?'default':$_REQUEST['campaign'];
+$campaignName = 'undetected';
+$carrierName = $carrierRow['carrier'];
+try {
+    $campaigns = new Campaigns();
+    $campaignRow = $campaigns->getByName($campaign);
+    $campaignName = $campaignRow['campaign'];
+    $cc = new CarrierCampaign();
+    $ccRow = $cc->getCC($carrierRow['id'], $campaignRow['id']);
+    $url = $ccRow['redirect_url'];
+} catch (Exception $e) {
+    $url = $carrierRow['redirect_url'];
+}
+add_to_log('[remote_addr='.$_SERVER['REMOTE_ADDR'].'][carrier='.$carrierName.'][campaign='.$campaignName.'][url='.$url.']', SEVERITY_DEBUG);
 header('Location: '.$url);
 ?>
