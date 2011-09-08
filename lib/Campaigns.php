75a16388efc90b08249a1223dc3e865629cce93a
Oleksandr Bodnarashyk <boda2004@gmail.com>
2011-09-08 15:08:34 +0300

    added getByName

--- lib/Campaigns.php
+++ lib/Campaigns.php
@@ -1,6 +1,7 @@
 <?php
 class Campaigns extends BaseClass {
     const SQL_CAMPAIGN_ROW = 'SELECT * FROM campaigns WHERE id=:id';
+    const SQL_CAMPAIGN_BY_NAME_ROW = 'SELECT * FROM campaigns WHERE campaign=:campaign';
     const SQL_CAMPAIGNS_ROWS = 'SELECT id, campaign, description FROM campaigns LIMIT :start, :limit';
     const SQL_CAMPAIGN_ADD = 'INSERT INTO campaigns (campaign, description) VALUES(:campaign, :description)';
     const SQL_CAMPAIGN_EDIT = 'UPDATE campaigns SET campaign=:campaign, description=:description WHERE id=:id';
@@ -9,6 +10,16 @@ class Campaigns extends BaseClass {
     public function getCampaign($id) {
         $sth = $this->sql->prepare(self::SQL_CAMPAIGN_ROW, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
         $sth->bindParam(':id', $id, PDO::PARAM_INT);
+        $sth->execute();
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        if (empty($row)) {
+            throw new Exception('Could not find campaign');
+        }
+        return $row;
+    }
+    public function getByName($campaign) {
+        $sth = $this->sql->prepare(self::SQL_CAMPAIGN_BY_NAME_ROW, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->bindParam(':campaign', $campaign, PDO::PARAM_INT);
         $sth->execute();
         $row = $sth->fetch(PDO::FETCH_ASSOC);
         if (empty($row)) {
